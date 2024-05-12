9362f644567a06b54f1ea7424a687f9498759850
mhok <edzone.ece@gmail.com>
2024-05-12 13:16:27 +0800

    home_imp

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,10 +1,15 @@
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
+import Partners from "./components/Partners";
+import Roadmap from "./components/Roadmap";
 
 export default function Home() {
   return (
     <>
       <Hero />
-
+      <Roadmap />
+      <Partners />
+      <Footer />
     </>
   );
 }
